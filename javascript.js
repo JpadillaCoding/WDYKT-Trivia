a4cabd70e6643afe5bf8d160a5551e6db86de51a
Jose Padilla <jose.apadilla99@gmail.com>
2023-01-04 15:04:34 -0600

    bug: Javascript isnt working on third html page

--- javascript.js
+++ javascript.js
@@ -3,8 +3,10 @@ let indexButton = document.querySelector('.index-btn')
 indexButton.onclick = function() {
     location.href = 'instructions.html'
 }
+
 let instButton = document.querySelector('.inst-btn')
 
 instButton.onclick = function() {
     location.href = 'game.html'
 }
+console.log(instButton)
