f74af505c019b3f4ff8e7467639f2d86e1352986
Jose Padilla <jose.apadilla99@gmail.com>
2023-01-04 14:33:10 -0600

    Feat: Instructions page along with button to the game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
-function loadPage() {
-    window.open('instructions.html')
+let indexButton = document.querySelector('.index-btn')
+
+indexButton.onclick = function() {
+    location.href = 'instructions.html'
+}
+let instButton = document.querySelector('.inst-btn')
+
+instButton.onclick = function() {
+    location.href = 'game.html'
 }
