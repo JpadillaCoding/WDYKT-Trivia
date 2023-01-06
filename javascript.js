f817e1581740bc3506ecfa751d7e2237158fb67d
Jose Padilla <jose.apadilla99@gmail.com>
2023-01-05 19:58:45 -0600

    feat: Added all skeleton layout for questions. question stay hidden unless active

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
+//loads the right js for the file the user is on.
 let page = document.body.className
+let answer = document.querySelector('.game-question')
+let next = document.querySelector('.next')
 switch(page) {
     case 'index-body':
         indexPage();
@@ -9,6 +12,7 @@ switch(page) {
 
 function indexPage() {
     let indexButton = document.querySelector('.index-btn')
+
     indexButton.onclick = function() {
         location.href = 'instructions.html'
     }
@@ -20,5 +24,13 @@ function instPage() {
     instButton.onclick = function() {
         location.href = 'game.html'
     }
-    console.log(instButton)
+}
+answer.addEventListener('click', clicked)
+function clicked(e) {
+    console.log(e)
+        if(e.target.localName =='button'){
+            if(e.target.className=='btn correct'){
+                next.style.display =' block'
+            }
+        }
 }
