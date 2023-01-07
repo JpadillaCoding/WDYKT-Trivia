2a2532105b251981b12da1ecb46dfb9da5c87537
Jose Padilla <jose.apadilla99@gmail.com>
2023-01-06 20:32:45 -0600

    feat: accuracy counter added

--- javascript.js
+++ javascript.js
@@ -3,7 +3,12 @@ let page = document.body.className
 let answer = document.querySelector('.game-question')
 let next = document.querySelector('.next')
 let slidesArg = document.querySelectorAll('.question')
+let counter = document.querySelector('.counter')
 let questionNum = 0
+let total = slidesArg.length-2
+let correct = 0
+
+
 switch(page) {
     case 'index-body':
         indexPage();
@@ -29,11 +34,14 @@ function instPage() {
 }
 answer.addEventListener('click', response)
 function response(e) {
-
+/*         Changes the buttons color depending if it was a correct class or not. 
+        adds to accuracy when correct button is added */
         if(e.target.localName =='button'){
             if(e.target.className=='btn correct'){
                 e.target.style.backgroundColor='rgba(34, 251, 46, 0.8)'
                 next.style.display='block'
+                correct ++
+                counter.innerHTML =`Accuracy:${Math.floor((correct/total)*100)}`
             }
             else {
                 e.target.style.backgroundColor='rgba(255, 72, 72, 0.8)'
@@ -46,9 +54,12 @@ function response(e) {
 }
 next.addEventListener('click', nextPg)
 function nextPg() { 
-    console.log(questionNum, slidesArg.length)
+    if ((questionNum + 1) < total) {
     next.style.display='none'
     slidesArg[questionNum].classList.remove('active')
     questionNum ++
     slidesArg[questionNum].classList.add('active')
+    }
 }
+//modify the accuracy counter
+//use the accuracy counter to add into the innerhtml
